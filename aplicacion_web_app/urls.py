7a026e5961b25c02b25cd5f9b1e8573a02c494d2
AlexanderOshita <aoshita@idegostd.com>
2024-07-28 19:03:56 -0500

    update code

--- aplicacion_web_app/urls.py
+++ aplicacion_web_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from . import views, machinelearning
 
 urlpatterns = [
-    # path('',views.home,name='home')
-    path('MachineLearning/', views.MachineLearning, name='MachineLearning'),
+    #path('',views.login,name='login')
+    path('MachineLearning/', machinelearning.MachineLearning, name='MachineLearning'),
 ]
