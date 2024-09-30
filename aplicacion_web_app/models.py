cac8db2723efea24b512a0b00c7f842d6ceacf3f
AlexanderOshita <aoshita@idegostd.com>
2024-09-30 01:17:36 -0500

    added tables

--- aplicacion_web_app/models.py
+++ aplicacion_web_app/models.py
@@ -3,10 +3,78 @@ from django.db import models
 
 class auth_user(models.Model):
     id=models.BigAutoField(primary_key=True)
-    password=models.CharField(max_length=128,null=False,blank=True)
+    password=models.CharField(max_length=128, null=False, blank=True)
     last_login =models.DateTimeField(null=True, blank=True)
-    username =models.CharField(max_length=150, null=False,blank=True)
-    email = models.CharField(max_length=150, null=False,blank=True)
+    username =models.CharField(max_length=150, null=False, blank=True)
+    email = models.CharField(max_length=150, null=False, blank=True)
     class Meta:
         managed = False
         db_table = 'auth_user'
+
+class blacklist(models.Model):
+    id_blacklist=models.IntegerField(primary_key=True)
+    IP=models.CharField(max_length=100, null=False, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'blacklist'
+
+class data_ransomware(models.Model):
+    id_data_ransomware=models.IntegerField(primary_key=True)
+    id_user_id=models.ForeignKey('auth_user', on_delete=models.CASCADE)
+    id_blacklist_id=models.ForeignKey('blacklist', on_delete=models.CASCADE)
+    timestamp_s=models.BigIntegerField(null=True, blank=True)
+    timestamp_ms=models.BigIntegerField(null=True, blank=True)
+    lba=models.FloatField(null=True, blank=True)
+    block_size=models.FloatField(null=True, blank=True)
+    entropy_shannon=models.FloatField(null=True, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'data_ransomware'
+
+class detection(models.Model):
+    id_detection=models.IntegerField(primary_key=True)
+    id_data_ransomware_id=models.ForeignKey('data_ransomware', on_delete=models.CASCADE)
+    ransomware_detected=models.BooleanField(default=False)
+    ransomware_type=models.CharField(max_length=50, null=False, blank=True)
+    percentage_reliability=models.FloatField(null=True, blank=True)
+    detection_date=models.DateField(null=True, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'detection'
+
+class detector_rf(models.Model):
+    id_detector_rf=models.IntegerField(primary_key=True)
+    id_detection_id=models.ForeignKey('detection', on_delete=models.CASCADE)
+    precision=models.FloatField(null=True, blank=True)
+    accuracy=models.FloatField(null=True, blank=True)
+    recall=models.FloatField(null=True, blank=True)
+    f1_score=models.FloatField(null=True, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'detector_rf'
+
+class detector_xgboost(models.Model):
+    id_detector_xgb=models.IntegerField(primary_key=True)
+    id_detection_id=models.ForeignKey('detection', on_delete=models.CASCADE)
+    precision=models.FloatField(null=True, blank=True)
+    accuracy=models.FloatField(null=True, blank=True)
+    recall=models.FloatField(null=True, blank=True)
+    f1_score=models.FloatField(null=True, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'detector_xgboost'
+
+class response(models.Model):
+    id_response=models.IntegerField(primary_key=True)
+    id_detection_id=models.ForeignKey('detection', on_delete=models.CASCADE)
+    action=models.CharField(max_length=500, null=False, blank=True)
+    detail=models.CharField(max_length=800, null=False, blank=True)
+    response_date=models.DateField(null=True, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'response'
+
+class logs(models.Model):
+    id_logs=models.IntegerField(primary_key=True)
+    id_detection_id=models.ForeignKey('detection', on_delete=models.CASCADE)
+    id_response_id=models.ForeignKey('response', on_delete=models.CASCADE)
