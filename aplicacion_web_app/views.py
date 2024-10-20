e9871ec84ce61e51a86b7dcd0409de1cb7447c2f
Rod22441 <bulldog22441@gmail.com>
2024-10-19 22:18:46 -0500

    changes view.py

--- aplicacion_web_app/views.py
+++ aplicacion_web_app/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.utils.translation import gettext as _
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -12,7 +13,8 @@ from datetime import datetime
 
 import joblib
 import numpy as np
-
+import openpyxl
+import pandas as pd
 def loginUser(request):
     '''
         - Login the User
@@ -42,29 +44,29 @@ def sign_up(request):
         - Save the Data the new User in Database PostgreSQL
     '''
     if request.method == 'POST':
-        usuario_nuevo = request.POST.get('username')
-        correo = request.POST.get('email')
-        pass_nuevo = request.POST.get('password')
-        pass_confirmacion = request.POST.get('confpassword')
+        usuario_nuevo = request.POST.get('username').strip()
+        correo = request.POST.get('email').strip()
+        pass_nuevo = request.POST.get('password').strip()
+        pass_confirmacion = request.POST.get('confpassword').strip()
 
         #VALIDATION
         if not all([usuario_nuevo, correo, pass_nuevo, pass_confirmacion]):
-            context = {'error_message': 'Todos los campos son obligatorios'}
+            context = {'error_message': _('Todos los campos son obligatorios')}
             return render (request, 'appweb/signup.html', context)
         if pass_nuevo != pass_confirmacion:
-            context = {'error_message': 'Las contraseñas no coinciden'}
+            context = {'error_message': _('Las contraseñas no coinciden')}
             return render (request, 'appweb/signup.html', context)
         try:
             validate_email(correo)
         except ValidationError:
-            context = {'error_message': 'El correo electrónico no es válido'}
+            context = {'error_message': _('El correo electrónico no es válido')}
             return render (request, 'appweb/signup.html', context)
         if len(pass_nuevo) < 8:
-            context = {'error_message': 'La contraseña debe tener al menos 8 caracteres'}
-            return render (request, 'appweb/signup.html', context)
+            context = {'error_message': _('La contraseña debe tener al menos 8 caracteres')}
+            print(f"Mensaje de error a mostrar: '{context['error_message']}'")
+            return render(request, 'appweb/signup.html', context)
         if auth_user.objects.filter(username=usuario_nuevo).exists() or auth_user.objects.filter(email=correo).exists():
-            print('HOLA')
-            context = {'error_message': 'El nombre de usuario o el correo electrónico ya están en uso'}
+            context = {'error_message': _('El nombre de usuario o el correo electrónico ya están en uso')}
             return render (request, 'appweb/signup.html', context)
         
        # SAVE IN DATABASE
@@ -75,7 +77,7 @@ def sign_up(request):
                 password=pass_nuevo
             )
             user.save()  # Saves the user in PostgreSQL
-            messages.success(request, 'Se registró correctamente')
+            messages.success(request, _('Se registró correctamente'))
             return redirect('login')
         except Exception as e:
             print('No se pudo registrar. Error: ', e)
@@ -98,6 +100,8 @@ def predicRansomware(request):
     '''
     rfModel = joblib.load('random_forest_model.pkl')
     xgbModel = joblib.load('xg_boost_model.pkl')
+    label_encoder = joblib.load('label_encoder.pkl')
+    
     if request.method == 'POST':
         timestampS = request.POST.get('Timestamp_s')
         timestampMS = request.POST.get('Timestamp_ms')
@@ -105,7 +109,7 @@ def predicRansomware(request):
         size = request.POST.get('Size')
         entropy = request.POST.get('Entropy')
         IP = request.POST.get('IP')
-
+        
         predictedFinal = 0
         date = datetime.today().date().strftime('%m/%d/%Y')
 
@@ -121,7 +125,7 @@ def predicRansomware(request):
 
         # Verificar que todos los campos estén llenos
         if not (timestampS and timestampMS and LBA and size and entropy):
-            context = {"error_message": "Tienes que completar todos los campos."}
+            context = {"error_message": _("Tienes que completar todos los campos.")}
             return render(request, 'appweb/detection/detection.html', context)
 
         try:
@@ -132,33 +136,124 @@ def predicRansomware(request):
             size = float(size)
             entropy = float(entropy)
         except ValueError:
-            context = {"error_message": "Por favor, ingresa valores válidos en todos los campos."}
+            context = {"error_message": _("Por favor, ingresa valores válidos en todos los campos.")}
             return render(request, 'appweb/detection/detection.html', context)
 
         # Verify data with the ML Model
         inputData = np.array([[timestampS, timestampMS, LBA, size, entropy]])
         
+        '''
+        Probabilidad estimada por cada modelo (Random Forest y XGBoost):
+        Explicación:
+            Se calcula la probabilidad final combinada de ambos modelos. Se muestrar estas probabilidades por separado como un valor cuantitativo que respalda la decisión. 
+        Ejemplo:
+            "Random Forest estima una probabilidad de detección del X%, mientras que XGBoost estima una probabilidad del Y%".
+        '''
         # Prediction Model Random Forest
-        predictionRF = rfModel.predict(inputData)
         probabilitiesRF = rfModel.predict_proba(inputData)
         predictedProbabilityRF = np.max(probabilitiesRF) * 100
-        print(predictedProbabilityRF)
+        typeRansomwareRF = rfModel.predict(inputData)
         
         # Prediction Model Extreme Gradient Boosting
-        predictionXGB = xgbModel.predict(inputData)
         probabilitiesXGB = xgbModel.predict_proba(inputData)
         predictedProbabilityXGB = np.max(probabilitiesXGB) * 100
-        print(predictedProbabilityXGB)
+        preTypeRansomwareXGB = xgbModel.predict(inputData)
+        typeRansomwareXGB = label_encoder.inverse_transform(preTypeRansomwareXGB)
         
-        # Promedio Simple
-        predictedFinal = round((predictedProbabilityRF + predictedProbabilityXGB) / 2, 2)
+        '''
+        Contribución de características:
+            Explicación de la metrica:
+                La importancia de características del modelo de Random Forest y Extreme Gradient Boosting es para mostrar al usuario la predicción. Esto quiere decir lo que muestra es qué características (entropía, tamaño del bloque, LBA) tuvieron más peso en la predicción, lo que permite proporcionar al usuario una explicación cuantitativa del porcentaje.
+            Ejemplo:
+                "El tamaño del bloque (Size) contribuyó un X% en la decisión final debido a su valor elevado".
+                "La entropía (Entropy) influyó un Y% en la predicción debido a la alta dispersión observada".
+        '''
+        # Importancia de características de Random Forest 
+        rfFeatureImportances = rfModel.feature_importances_
+        important_features_rf = {
+            "Entropy": round(rfFeatureImportances[4] * 100, 2),
+            "Size": round(rfFeatureImportances[3] * 100, 2),
+            "LBA": round(rfFeatureImportances[2] * 100, 2)
+        }
 
-        context = {'ransomware_type': predictionRF[0],
-                   'probability': predictedFinal,
-                   'date': date
-                }
+        # Feature importances for XGB
+        xgbFeatureImportances = xgbModel.feature_importances_
+        important_features_xgb = {
+            "Entropy": round(xgbFeatureImportances[4] * 100, 2),
+            "Size": round(xgbFeatureImportances[3] * 100, 2),
+            "LBA": round(xgbFeatureImportances[2] * 100, 2)
+        }
 
-    return render(request,'appweb/detection/detection.html',context)
+        '''
+        Desviación o anomalía en las características:
+        Explicación
+            Para cada entrada proporcionada, se calcula la desviación respecto a un valor promedio de las características normales. Así, se justifica el porcentaje en base a cuán lejos están los valores del comportamiento esperado (valores típicos de ransomware vs normales).
+        Ejemplo:
+            "La entropía observada es un Z% mayor que el promedio de los archivos normales, lo que contribuye a la alta probabilidad de ransomware".
+        '''
+        # Analyze deviations (anomalies)
+        deviation_entropy = round(abs(entropy - 0.95) * 100, 2)  # 0.95 as average entropy threshold
+        deviation_size = round(abs(size - 1024) / 100, 2) # 1024 as a threshold for block size
+        deviation_LBA = round(abs(LBA - 600000), 2)  # LBA threshold
+        
+        # Final prediction
+        if typeRansomwareRF[0] == typeRansomwareXGB[0]:
+            ransomwareType = typeRansomwareXGB[0]
+            # Promedio Simple
+            predictedFinal = round((predictedProbabilityRF + predictedProbabilityXGB) / 2, 2)
+        elif predictedProbabilityRF > predictedProbabilityXGB:
+            ransomwareType = typeRansomwareRF[0]
+            predictedFinal = round(predictedProbabilityRF, 2)
+        else:
+            ransomwareType = typeRansomwareXGB[0]
+            predictedFinal = round(predictedProbabilityXGB, 2)
 
-def responseRansomware(request):
-    return
+        # Evaluate final confidence and response
+        if predictedFinal > 50:
+            features = {'entropy': entropy, 'size': size, 'LBA': LBA}
+            mensaje, motive = responseRansomware(predictedFinal, features)
+        else:
+            motive = _('Sin respuesta contra la amenaza')
+        
+        print (important_features_rf, '-', important_features_xgb, '-', deviation_entropy, '-', deviation_size, '-', deviation_LBA)
+        context = {
+            'ransomware_type': ransomwareType,
+            'probability': predictedFinal,
+            'date': date,
+            'motive': motive,
+            'important_features_rf': important_features_rf,
+            'important_features_xgb': important_features_xgb,
+            'deviation_entropy': deviation_entropy,
+            'deviation_size': deviation_size,
+            'deviation_LBA': deviation_LBA
+        }
+
+    return render(request, 'appweb/detection/detection.html', context)
+
+def responseRansomware(predictedFinal, features):
+    '''
+        - Analisis de umbrales de cada caracteristicas (Anomalia)
+    '''
+    motivos = []
+    if features['entropy'] > 0.95:
+        motivos.append(_("Se detectó anomalía en la entropía de Shannon"))
+    
+    if features['size'] > 1024:
+        motivos.append(_("El tamaño del bloque es inusualmente grande"))
+    
+    if features['LBA'] > 500000:
+        motivos.append(_("Acceso a bloques sospechosos de memoria"))
+    
+    if not motivos:
+        motivos.append(_("No se detectaron anomalías significativas en las características observadas"))
+
+    mensaje = f"El documento fue puesto en cuarentena. Motivos:\n"
+    for motivo in motivos:
+        mensaje += f"- {motivo}\n"
+    
+    return mensaje, motivos
+
+@login_required
+def excelDetail():
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
